e031c2580c568193984460dafc53d3fb638f918d
Kyoungwon Min <mkkw2047@gmail.com>
2021-11-19 17:25:53 +0900

    전체+월별 랭킹

--- entire_ranking.php
+++ entire_ranking.php
@@ -1,34 +1,86 @@
-<?php
-    $mysqli = mysqli_connect("localhost", "team01", "team01", "team01");
+<html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
 
-    if(mysqli_connect_errno())
-    {
-        printf("Connect failed: %s\n", mysqli_connect_error());
-        exit();
+  <?php
+      $mysqli = mysqli_connect("localhost", "team01", "team01", "team01");
+
+      if(mysqli_connect_errno())
+      {
+          printf("Connect failed: %s\n", mysqli_connect_error());
+          exit();
     }
 
-    else{
-        $sql = "select dense_rank() over (order by count(A.menu_id) desc) as ranking, B.menu_name from top A join menu B on A.menu_id=B.menu_id group by A.menu_id";
-        $res = mysqli_query($mysqli, $sql);
-        if($res){
-            echo "Entire Service Area Food Ranking<br/>";
-            echo"ranking | menu name</br>";
+      else{
+          $sql = "select dense_rank() over (order by count(A.menu_id) desc) as ranking, B.menu_name from top A join menu B on A.menu_id=B.menu_id group by A.menu_id";
+          $res = mysqli_query($mysqli, $sql);
+          if($res){
+              echo "Entire Service Area Food Ranking<br/>";
+              echo"ranking | menu name</br>";
 
-            while($newArray = mysqli_fetch_array($res, MYSQLI_ASSOC)){
-                $ranking = $newArray['ranking'];
-                $menu_name = $newArray['menu_name'];
-                //echo $ranking." ".$menu_name."<br/>";
-                printf("%02d %s", $ranking, $menu_name);
+              $idx=1;
+              while($newArray = mysqli_fetch_array($res, MYSQLI_ASSOC)){
+
+                  $ranking = $newArray['ranking'];
+                  $menu_name = $newArray['menu_name'];
+                  //echo $ranking." ".$menu_name."<br/>";
+                  printf("%02d %s", $ranking, $menu_name);
+
+                  $idx++;
+
+                  if($idx%5==0){
+                    echo "<br/>";
+                  }
+
+              }
+              echo "<br/>";
+          }
+
+          else{
+              printf("Could not retreive records: %s\n", mysqli_error($mysqli));
+
+          }
+
+          mysqli_free_result($res);
+
+          $monthly = array("January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December");
+          for($i=1; $i<=12; $i++){
+              $sql = "select dense_rank() over (order by count(A.menu_id) desc) as ranking, B.menu_name from top A join menu B on A.menu_id=B.menu_id where A.month_code=$i group by A.menu_id";
+              $res = mysqli_query($mysqli, $sql);
+
+              if($res){
                 echo "<br/>";
-            }
-        }
+                echo "Monthly Service Area Food Ranking<br/>";
+                //echo "<br/>";
+                echo $monthly[$i-1]."</br>";
+                echo"ranking | menu name</br>";
 
-        else{
-            printf("Could not retreive records: %s\n", mysqli_error($mysqli));
+                $idx=1;
 
-        }
+                while($newArray = mysqli_fetch_array($res, MYSQLI_ASSOC)){
+                    $ranking = $newArray['ranking'];
+                    $menu_name = $newArray['menu_name'];
 
-        mysqli_free_result($res);
-        mysqli_close($mysqli);
-    }
-?>
+                    printf("%02d %s", $ranking, $menu_name);
+
+                    $idx++;
+
+                    if($idx%5==0){
+                      echo "<br/>";
+                    }
+                }
+                echo "<br/>";
+              }
+          }
+
+
+          mysqli_close($mysqli);
+      }
+  ?>
+
+  </body>
+</html>
